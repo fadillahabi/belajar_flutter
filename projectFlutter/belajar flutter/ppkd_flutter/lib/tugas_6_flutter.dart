8c2de0bab126db05bef038b9b7b89a44688477a6
fadillahabi <fadillahabiprayogo@gmail.com>
2025-05-25 21:36:27 -0700

    Tugas 6 Flutter : Done

--- projectFlutter/belajar flutter/ppkd_flutter/lib/tugas_6_flutter.dart
+++ projectFlutter/belajar flutter/ppkd_flutter/lib/tugas_6_flutter.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class TugasEnamFlutter extends StatefulWidget {
@@ -8,144 +9,233 @@ class TugasEnamFlutter extends StatefulWidget {
 }
 
 class _TugasEnamFlutterState extends State<TugasEnamFlutter> {
+  final passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff00224F),
+      backgroundColor: const Color(0xff00224F),
       appBar: AppBar(
         toolbarHeight: 120,
-        backgroundColor: Color(0xff00224F),
+        backgroundColor: const Color(0xff00224F),
         leading: Padding(
           padding: const EdgeInsets.only(left: 20),
           child: IconButton(
-            icon: Icon(Icons.arrow_back_ios),
+            icon: const Icon(Icons.arrow_back_ios),
             onPressed: () {},
             color: Colors.white,
           ),
         ),
-        title: Text(
+        title: const Text(
           "Login",
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700),
         ),
         centerTitle: true,
       ),
-
-      body: Center(
-        child: Column(
-          children: [
-            Text(
-              "Hello Welcome Back",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 26,
-                fontWeight: FontWeight.w600,
-                fontFamily: "Roboto",
-              ),
-            ),
-            SizedBox(height: 8),
-            SizedBox(
-              width: 140,
-              height: 42,
-              // color: Colors.white,
-              child: Text(
-                "Welcome back please sign in again",
-
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 12),
+              const Text(
+                "Hello Welcome Back",
                 style: TextStyle(
-                  color: Color(0x40ffffff),
-                  fontSize: 14,
+                  color: Color(0xffFFFFFF),
+                  fontSize: 26,
+                  fontWeight: FontWeight.w600,
                   fontFamily: "Roboto",
-                  fontWeight: FontWeight.w400,
                 ),
-                textAlign: TextAlign.center,
               ),
-            ),
-            SizedBox(height: 88),
-            SizedBox(
-              width: 327,
-              height: 402,
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.email, color: Color(0xffC4C4C4)),
-                      hintText: "Email",
-                      hintStyle: TextStyle(fontSize: 14, color: Colors.white),
-                      filled: true,
-                      fillColor: Color(0xff00224F),
-                      border: UnderlineInputBorder(
-                        //Border awal
-                        // borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide(
-                          color: Color(0xff12325D),
-                          width: 1,
-                        ),
-                        // borderSide: BorderSide(color: Colors.black, width: 5),
-                      ),
-                      focusedBorder: UnderlineInputBorder(
-                        // BOrder Ketika di klik
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide(color: Colors.black, width: 1),
-                        // borderSide: BorderSide(color: Colors.black, width: 5),
-                      ),
-                      // border
+              const SizedBox(height: 8),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 92, vertical: 8),
+                child: Text(
+                  "Welcome back please sign in again",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(0x40ffffff),
+                    fontSize: 14,
+                    fontFamily: "Roboto",
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 56),
+              TextField(
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.email, color: Color(0xffC4C4C4)),
+                  hintText: "Fadillah Abi Prayogo",
+                  hintStyle: const TextStyle(fontSize: 14, color: Colors.white),
+                  filled: true,
+                  fillColor: const Color(0xff00224F),
+                  border: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xff12325D), width: 1),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Color(0xff12325D),
+                      width: 1,
                     ),
                   ),
-                  SizedBox(height: 35),
-                  TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.lock, color: Color(0xffC4C4C4)),
-                      hintText: "Password",
-                      hintStyle: TextStyle(fontSize: 14, color: Colors.white),
-                      filled: true,
-                      fillColor: Color(0xff00224F),
-                      border: UnderlineInputBorder(
-                        //Border awal
-                        // borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide(
-                          color: Color(0xff12325D),
-                          width: 1,
-                        ),
-                        // borderSide: BorderSide(color: Colors.black, width: 5),
-                      ),
-                      focusedBorder: UnderlineInputBorder(
-                        // BOrder Ketika di klik
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide(color: Colors.black, width: 1),
-                        // borderSide: BorderSide(color: Colors.black, width: 5),
-                      ),
-                      // border
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.black, width: 1),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 35),
+              TextField(
+                controller: passwordController,
+                onChanged: (value) {
+                  print(value);
+                  setState(() {
+                    passwordController.text = value;
+                  });
+                },
+                onSubmitted: (value) {
+                  print(value);
+                },
+                obscuringCharacter: "*",
+                style: TextStyle(color: Colors.white),
+                obscureText: true,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.lock, color: Color(0xffC4C4C4)),
+                  hintText: "Password",
+                  hintStyle: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                  filled: true,
+                  fillColor: const Color(0xff00224F),
+                  border: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xff12325D), width: 1),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Color(0xff12325D),
+                      width: 1,
                     ),
                   ),
-                  SizedBox(height: 35),
-                  SizedBox(
-                    width: 287,
-                    height: 56,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        foregroundColor: Color(0xff00224F),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(32),
-                        ),
-                      ),
-                      onPressed: () {
-                        // Kode yang akan dijalankan saat tombol ditekan
-                        print('Tombol ditekan!');
-                      },
-                      child: const Text(
-                        'Login',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w700,
-                          fontFamily: "Roboto",
-                        ),
-                      ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.black, width: 1),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 35),
+              SizedBox(
+                width: double.infinity,
+                height: 56,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: const Color(0xff00224F),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(32),
                     ),
                   ),
+                  onPressed: () {
+                    print('Tombol Login ditekan!');
+                  },
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                      fontFamily: "Roboto",
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+              Row(
+                children: const [
+                  Expanded(child: Divider(color: Color(0xff12325E))),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Text("or", style: TextStyle(color: Colors.white)),
+                  ),
+                  Expanded(child: Divider(color: Color(0xff12325E))),
                 ],
               ),
-            ),
-          ],
+              const SizedBox(height: 23),
+
+              // Facebook Button
+              SizedBox(
+                width: double.infinity,
+                height: 48,
+                child: ElevatedButton.icon(
+                  icon: const Icon(
+                    Icons.facebook_rounded,
+                    size: 18,
+                    color: Color(0xFFFFFFFF),
+                  ),
+                  label: const Text('Facebook'),
+                  onPressed: () {
+                    print("Login Facebook");
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xff11325C),
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(22),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+
+              // Gmail Button
+              SizedBox(
+                width: double.infinity,
+                height: 48,
+                child: ElevatedButton.icon(
+                  icon: Image.asset(
+                    "assets/images/iconGoogle.png",
+                    width: 18,
+                    height: 18,
+                  ),
+                  label: const Text('Gmail'),
+                  onPressed: () {
+                    print("Login Google Mail");
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xff11325C),
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(22),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 12),
+              RichText(
+                text: TextSpan(
+                  text: "Already have an account ?",
+                  style: TextStyle(
+                    color: Color(0xff999999),
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: "  Sign In",
+                      style: TextStyle(
+                        color: Color(0xff3C7EEE),
+                        fontWeight: FontWeight.w700,
+                        fontSize: 14,
+                      ),
+                      recognizer:
+                          TapGestureRecognizer()
+                            ..onTap = () => print("Sing In"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
